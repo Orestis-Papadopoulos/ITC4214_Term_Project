abae1c92a1c979618f32d10d32733dcb6fa103fb
Orestis Papadopoulos <o.papadopoulos@acg.edu>
2022-11-29 18:11:49 +0200

    Started the search results page.

--- LegoGroup/catalog/urls.py
+++ LegoGroup/catalog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views # views are in this directory
 
 urlpatterns = [
-    path('', views.base, name = "base")
+    path('', views.base, name = "base"),
+    path('lego_parts/', views.LegoPartListView.as_view(), name = "lego_parts")
 ]
--- LegoGroup/catalog/views.py
+++ LegoGroup/catalog/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
+from .models import *
+from django.views import generic
 
-# home page
 def base(request):
     return render(request, "base.html")
+
+def index(request):
+    pass
 
 def about(request):
     pass
 
-def search_results(request):
-    pass
+def lego_parts(request):
+    lego_parts_count = LegoPart.objects.count() # .all() is implied by default
+
+    context = {
+        'lego_parts_count': lego_parts_count
+    }
+
+    return render(request, 'lego_parts.html', context = context)
 
 def login(request):
     pass
@@ -19,3 +29,6 @@ def create_account(request):
 # redirects to the 'create_account' page where the fields contain the user's info?
 def user_profile(request):
     pass
+
+class LegoPartListView(generic.DetailView):
+    model = LegoPart
